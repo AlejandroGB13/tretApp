4d76122044ae98339698e4d5f80de41e2efd9999
DavidGrifoGimeno <al385992@uji.es>
2022-02-17 17:39:50 +0100

    intento splash screen

--- app/src/main/java/com/example/tretapp/loginActivity.java
+++ app/src/main/java/com/example/tretapp/loginActivity.java
@@ -3,8 +3,14 @@ package com.example.tretapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class loginActivity extends AppCompatActivity {
+    private EditText emailEditText = findViewById(R.id.emailEditText);
+    private EditText pwdEditText = findViewById(R.id.pwdEditText);
+    private Button accedirButton = findViewById(R.id.accedirButton);
+    private Button recuperarPwdButton = findViewById(R.id.recuperaPwdButton);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
